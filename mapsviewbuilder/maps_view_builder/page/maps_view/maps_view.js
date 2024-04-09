e6cc2a95a8bb22e9a7718d206c32f757c5989142
aashishv09 <ashish@extensioncrm.com>
2024-04-09 12:03:34 +0000

    changed leaflet css url from cdn to local

--- mapsviewbuilder/maps_view_builder/page/maps_view/maps_view.js
+++ mapsviewbuilder/maps_view_builder/page/maps_view/maps_view.js
@@ -4,7 +4,8 @@ frappe.pages['maps-view'].on_page_load = async function (wrapper) {
 		title: 'Maps View',
 		single_column: true
 	});
-	await frappe.require("https://unpkg.com/leaflet@1.9.4/dist/leaflet.css")
+	// await frappe.require("https://unpkg.com/leaflet@1.9.4/dist/leaflet.css")
+	await frappe.require("leaflet.bundle.css")
 	await frappe.require("maps_view.bundle.js")
 	console.log(frappe.mapsviewbuilder)
 	const mapsview = new frappe.mapsviewbuilder.MapsViewBuilder(page, wrapper)
